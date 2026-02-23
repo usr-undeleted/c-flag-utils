38a9f116eed80569e568c5dbfb23174834df4453
Undeleted <zephimhere@gmail.com>
2026-02-23 12:51:54 -0300

    General fixes in example code and a bit of the func

--- src/example.c
+++ src/example.c
@@ -5,11 +5,15 @@
 void fhelp(int num); /* -h and --help */
 
 int main (int argc, char *argv[]) {
-	int arrPointer[argc];
-	memset(arrPointer, -1, sizeof(arrPointer));
+	/* the array to be edited */
+	int arr[argc];
+	/* function usage: */
+	mapflags(argc,argv,arr);
+
+	/* array will be mapped out following the args */
 
 		for (int i = 0; i < argc; i++) {
-			printf("Argc %d (%s) returned: %d\n", i, argv[i], arrPointer[i]);
+			printf("Argc %d (%s) returned: %d\n", i, argv[i], arr[i]);
 		}
 
 		/* ^ new func that uses all args, old func below */
--- src/flagutils.h
+++ src/flagutils.h
@@ -7,7 +7,7 @@
 /* this is all a bit over-commented because im scared i might forget how it works */
 
 int flagchk(int argc, char *argv[]) { /* only checks argv[1]*/
-	/* int used in returns, return -1 if failed, rest is added on */
+	/* -1 if fail, 0-x if succesful */
 	int flagVal = -1;
 
 	/* array that contains all flags used in the project this will be in
@@ -15,10 +15,10 @@ int flagchk(int argc, char *argv[]) { /* only checks argv[1]*/
 	const char *pflags[] = {"--help","-h"};
 	const int arrlen = sizeof(pflags) / sizeof(pflags[0]);
 
-	// argc check, returns 0 if there is just one arg
+	/* return -1  */
 	if (argc < 2) return flagVal;
 
-	/* while loop that compares flag arg to entries in pflags then updates and returns the appropriate flagval */
+	/* compares arg to all entries and returns val */
 	for (int i = 0; i < arrlen; i++) {
 		if (strcmp(argv[1], pflags[i]) == 0) {
 			flagVal = i;
@@ -27,11 +27,12 @@ int flagchk(int argc, char *argv[]) { /* only checks argv[1]*/
 	}
 	return flagVal;
 }
-// ^ old code, should only be used if you want more simplicity. will be kept here because im proud of it <3
+// ^ old code, only checks argv[1]
 
-int *mapflags(int argc, char *argv[], int arr[]) { /* make an array in heap memory containing entries, checks all args */
+int *mapflags(int argc, char *argv[], int arr[]) { /* get arr from caller */
 	const char *pflags[] = {"--help","-h"};
 	const int tflags = sizeof(pflags) / sizeof(pflags[0]);
+	memset(arr,-1,sizeof(*arr));
 
 	for (int i = 0; i < argc; i++) {
 		arr[i] = -1; /* by default, entry is -1 as it didnt find a match */
